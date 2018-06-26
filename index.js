8899e59ce4fa0975b36a92e2a072e2c981868165
yeerkkiller1 <yeerkkiller1@gmail.com>
2018-06-25 21:41:12 -0400

    Fix permissions.

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+var child_process = require("child_process");
+
+// We need to do this because npm and yarn aren't setting permissions properly.
+//  Also, we need to exec, as fs.chmodSync sets the permissions on other? Not the current user? Wtf...
+
+child_process.execFileSync("chmod", ["a+x", getX264Path()]);
+
 function getX264Path() {
     return __dirname + "/x264";
 }
