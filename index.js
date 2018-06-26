4429851a2a9afd230e0bf3bf3083e2b130c4569b
yeerkkiller1 <yeerkkiller1@gmail.com>
2018-06-25 20:29:16 -0400

    Executable permissions.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-child_process.execFileSync("chmod", ["a+x", getX264Path()]);
-
-function getX264Path(...args) {
+function getX264Path() {
     return __dirname + "/x264";
-};
+}
 
 module.exports = getX264Path;
